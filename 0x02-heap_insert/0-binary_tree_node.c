1abed729cf279728d4065dc0689518efa9e25468
juanfe9118 <1168@holbertonschool.com>
2020-07-31 01:02:51 -0500

    Fix betty and modify parent assignment

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -8,16 +8,24 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
+	binary_tree_t *new;
 
-    new = malloc(sizeof(binary_tree_t));
-    if (!new)
-        return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
 
-    new->n = value;
-    new->parent = parent;
-    new->left = NULL;
-    new->right = NULL;
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
 
-    return (new);
+	if (!parent)
+		return (new);
+
+	if (!(parent->left))
+		parent->left = new;
+	else
+		parent->right = new;
+
+	return (new);
 }
