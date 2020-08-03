cb113fa4c529b26ba9eec29290a505f885138d81
juanfe9118 <1168@holbertonschool.com>
2020-08-02 23:58:59 -0500

    Add heap insert and update header file

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -23,6 +23,10 @@ typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+int binary_tree_height(const binary_tree_t *tree);
+int p_check(const binary_tree_t *tree, int h, int l);
+int heap_is_perfect(const binary_tree_t *tree);
+void heap_checker(heap_t **son, heap_t **father);
 heap_t *heap_insert(heap_t **root, int value);
 
 #endif /* _BINARY_TREES_H_ */
