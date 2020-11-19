314c49bce6b050ace51bfb69371a4c1f7b7ac9f3
juanfe9118 <1168@holbertonschool.com>
2020-11-18 23:16:45 -0500

    Fix semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -8,25 +8,25 @@ const FILMID = process.argv[2];
 const URL_BASE = 'https://swapi-api.hbtn.io/api/films';
 
 function doRequest (url) {
-    return new Promise(function (resolve, reject) {
-      request(url, function (error, res, body) {
-        if (!error && res.statusCode === 200) {
-          resolve(JSON.parse(body));
-        } else {
-          reject(error);
-        }
-      });
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (!error && res.statusCode === 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(error);
+      }
     });
+  });
+}
+
+// Usage:
+
+async function main (filmID) {
+  const res = await doRequest(`${URL_BASE}/${filmID}`);
+  for (const e of res.characters) {
+    const pj = await doRequest(e);
+    console.log(pj.name);
   }
-  
-  // Usage:
-  
-  async function main (filmID) {
-    const res = await doRequest(`${URL_BASE}/${filmID}`);
-    for (const e of res.characters) {
-      const pj = await doRequest(e);
-      console.log(pj.name);
-    }
-  }
-  
-  main(FILMID);
+}
+
+main(FILMID);
