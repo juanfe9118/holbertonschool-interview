51de3dd46cad9668ae72acba33348c04a258f000
juanfe9118 <1168@holbertonschool.com>
2020-11-18 23:15:46 -0500

    Update function

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -8,29 +8,25 @@ const FILMID = process.argv[2];
 const URL_BASE = 'https://swapi-api.hbtn.io/api/films';
 
 function doRequest (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, function (error, res, body) {
-      if (!error && res.statusCode === 200) {
-        resolve(JSON.parse(body));
-      } else {
-        reject(error);
-      }
+    return new Promise(function (resolve, reject) {
+      request(url, function (error, res, body) {
+        if (!error && res.statusCode === 200) {
+          resolve(JSON.parse(body));
+        } else {
+          reject(error);
+        }
+      });
     });
-  });
-}
-
-// Usage:
-
-async function main (filmID) {
-  const res = async () => await doRequest(`${URL_BASE}/${filmID}`);
-  res()
-    .then(async (r) => {
-      for (const e of r.characters) {
-        const { name } = await doRequest(e);
-        console.log(name);
-      }
-    })
-    .catch((e) => console.error(e));
-}
-
-main(FILMID).catch(e => { console.error(e); });
+  }
+  
+  // Usage:
+  
+  async function main (filmID) {
+    const res = await doRequest(`${URL_BASE}/${filmID}`);
+    for (const e of res.characters) {
+      const pj = await doRequest(e);
+      console.log(pj.name);
+    }
+  }
+  
+  main(FILMID);
