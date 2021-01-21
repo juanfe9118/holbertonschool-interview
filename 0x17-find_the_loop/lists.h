578c83ab59a59ddc58b9afbc343363c446e15186
juanfe9118 <1168@holbertonschool.com>
2021-01-21 01:16:35 -0500

    Remove unused prototypes

--- 0x17-find_the_loop/lists.h
+++ 0x17-find_the_loop/lists.h
@@ -20,8 +20,5 @@ typedef struct listint_s
 } listint_t;
 
 listint_t *find_listint_loop(listint_t *head);
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *head);
 
 #endif /* LISTS_H */
